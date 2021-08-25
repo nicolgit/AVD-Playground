7b5821c9a19f643281573dd31fd04c06887e583e
NicolD <nicola.delfino@outlook.com>
2021-08-25 15:09:04 +0200

    updated vnet and users creation

--- shell-script/02-vnet.sh
+++ shell-script/02-vnet.sh
@@ -2,6 +2,7 @@
 
 source 00-variables.sh
 
+#Create vnet
 az network vnet create \
   --name $VNET \
   --resource-group $RESOURCEGROUP \
@@ -10,8 +11,49 @@ az network vnet create \
   --subnet-name $SUBNETAD \
   --subnet-prefix $SUBNETADPREFIX
 
+#Create client subnet
 az network vnet subnet create \
   --resource-group $RESOURCEGROUP \
   --vnet-name $VNET \
   --name $SUBNETCLIENTS \
   --address-prefixes $SUBNETCLIENTSPREFIX
+
+#Create NSG 
+az network nsg create \
+  --name aadds-nsg \
+  --resource-group $RESOURCEGROUP
+ 
+#Create NSG rule "AllowRD"
+az network nsg rule create \
+  --resource-group $RESOURCEGROUP \
+  --name AllowRD \
+  --nsg-name aadds-nsg \
+  --access Allow \
+  --protocol TCP \
+  --direction Inbound \
+  --priority 201 \
+  --source-address-prefixes CorpNetSaw \
+  --source-port-ranges "*" \
+  --destination-address-prefixes "*" \
+  --destination-port-ranges 3389
+ 
+#Create NSG rule "Allow PS Remoting"
+az network nsg rule create \
+  --resource-group $RESOURCEGROUP \
+  --name AllowPSRemoting \
+  --nsg-name aadds-nsg \
+  --access Allow \
+  --protocol TCP \
+  --direction Inbound \
+  --priority 301 \
+  --source-address-prefixes AzureActiveDirectoryDomainServices \
+  --source-port-ranges "*" \
+  --destination-address-prefixes "*" \
+  --destination-port-ranges 5986
+ 
+# Associate the NSG with the VNET subnet
+az network vnet subnet update \
+  --resource-group $RESOURCEGROUP \
+  --name $SUBNETAD \
+  --vnet-name $VNET \
+  --network-security-group aadds-nsg
