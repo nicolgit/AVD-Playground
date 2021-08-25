ece247f845c1fc2cf25c1cd7974ca902231c9702
NicolD <nicola.delfino@outlook.com>
2021-08-25 18:42:41 +0200

    working on hostpool creation

--- shell-script/00-variables.sh
+++ shell-script/00-variables.sh
@@ -24,12 +24,3 @@ USER2UPN="$USER2@$AADDOMAIN"
 USER3UPN="$USER3@$AADDOMAIN"
 
 PASSWORD="pa.123.assword"
-
-ROMAPOOLNAME="RomaPool"
-ROMAWORKSPACE="RomaWorkspace"
-
-MILANOPOOLNAME="MilanoPool"
-MILANOWORKSPACE="MilanoWorkspace"
-
-VMTEMPLATEROMA= "{\"domain\":\"demo.nicold\",\"galleryImageOffer\":\"Windows-10\",\"galleryImagePublisher\":\"MicrosoftWindowsDesktop\",\"galleryImageSKU\":\"19h2-evd\",\"imageType\":\"Gallery\",\"imageUri\":null,\"customImageId\":null,\"namePrefix\":\"FirenzeVM\",\"osDiskType\":\"Standard_LRS\",\"useManagedDisks\":true,\"vmSize\":{\"id\":\"Standard_D2s_v3\",\"cores\":2,\"ram\":8},\"galleryItemId\":\"Microsoftwindowsdesktop.windows-1019h2-evd\"}"
-
--- shell-script/03-users.sh
+++ shell-script/03-users.sh
@@ -2,11 +2,14 @@
 
 source 00-variables.sh
 
-az ad user create \
+USER1JSON=$(az ad user create \
   --display-name $USER1 \
   --password $PASSWORD \
   --user-principal-name $USER1UPN \
-  --force-change-password-next-login false 
+  --force-change-password-next-login false \
+  -o json)
+
+USER1ID=$(echo $USER1JSON | jq '.objectId' | sed 's/"//g')
 
 az ad user create \
   --display-name $USER2 \
@@ -19,3 +22,16 @@ az ad user create \
   --password $PASSWORD \
   --user-principal-name $USER3UPN \
   --force-change-password-next-login false 
+
+az ad sp create-for-rbac --name "2565bd9d-da50-47d4-8b85-4c97f669dc36"
+
+GROUPID=$(az ad group create \
+  --display-name "AAD DC Administrators" \
+  --description "Delegated group to administer Azure AD Domain Services" \
+  --mail-nickname "AADDCAdministrators" \
+  -o json \
+  --query 'objectId' | sed 's/"//g')
+
+az ad group member add \
+  --group $GROUPID \
+  --member-id $USER1ID
