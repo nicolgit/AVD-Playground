b57642e33a6485c99e900897c9e9b1c581b3af70
Nicola Delfino <nicolgit@users.noreply.github.com>
2024-09-05 18:54:59 +0200

    step3 update

---
+++ v2/step2/01-users.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+az ad user create \
+  --display-name $USER1 \
+  --password $PASSWORD \
+  --user-principal-name $USER1UPN \
+  --force-change-password-next-sign-in false \
+  -o json
+
+az ad user create \
+  --display-name $USER2 \
+  --password $PASSWORD \
+  --user-principal-name $USER2UPN \
+  --force-change-password-next-sign-in false 
+
+az ad user create \
+  --display-name $USER3 \
+  --password $PASSWORD \
+  --user-principal-name $USER3UPN \
+  --force-change-password-next-sign-in false 
+
+USER1ID=$(az ad user show --id $USER1UPN | jq -r .id)
+az ad group member add --group "AAD DC Administrators"  --member-id $USER1ID
